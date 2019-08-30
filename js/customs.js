337172a1db8ccf69af72709983f5833dcb4d11c0
tareq0349 <tareqbhuiyan2021@gmail.com>
2019-08-30 17:31:07 +0600

    my portfolio

---
+++ js/customs.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+	$('.circleGraphic1').circleGraphic();
+	$('.circleGraphic2').circleGraphic({'color':'#E53238'});
+	$('.circleGraphic3').circleGraphic({'color':'#c1fc2f'});
+
+	$('.circleGraphic4').circleGraphic({'color':'#33f5fe'});
+
+}
